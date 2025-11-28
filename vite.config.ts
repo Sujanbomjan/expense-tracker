0be8a540445b712dc3fa5caa334449f6dfd57f51
Sujanbomjan <bomjan133@gmail.com>
2025-11-29 00:13:28 +0545

    added vercel.json while refresh its throws 404 error on vercel app

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
 });
